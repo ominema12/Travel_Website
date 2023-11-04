292df8f69df71fb37d75287235c0e61cb83b7654
ominema12 <ominema12@gmail.com>
2023-11-05 01:41:25 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from 'next'
+
+import './globals.css'
+
+import Footer from '@/components/Footer'
+import Navbar from '@/components/Navbar'
+
+
+
+export const metadata: Metadata = {
+  title: 'Travel',
+  description: 'Travel UI/UX app for camping',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar/>
+        <main className="relative overflow-hidden">
+          {children}
+        </main>
+        <Footer />
+      </body>
+    </html>
+  )
+}
+
---
+++ components/Navbar.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Link from "next/link"
+import Image from "next/image"
+import { NAV_LINKS } from '@/constants'
+
+const Navbar = () => {
+  return (
+        <nav className="flexBetween max-container padding-container relative z-30 py-5">
+            <Link href="/" >
+            <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
+              <ul className="hidden h-full gap-12 lg:flex">
+                {NAV_LINKS.map((link)=> (
+                    <Link href={link.href} key={link.key} 
+                    className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold">
+                        {link.label}
+                    </Link>
+                ))}
+              </ul>
+
+            </Link>
+
+        </nav>
+      )
+    }
+    
+    export default Navbar
+    
+    // <nav className="flex flex-row justify-content items-center mx-auto max-w-[1440px] bg-red-500 px-6 lg:px-20">
+    //     <div className="bg-yellow-500"></div>
+    //     <div className="bg-white-500"></div>
+    //     <div className="bg-blue-500"></div>
+    // </nav>
