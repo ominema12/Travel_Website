e3937469ac94bf685f1991e33a2bedb092193532
ominema12 <ominema12@gmail.com>
2023-11-05 02:25:26 +0530

    navbar update

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,7 +6,7 @@ import { NAV_LINKS } from '@/constants'
 const Navbar = () => {
   return (
         <nav className="flexBetween max-container padding-container relative z-30 py-5">
-            <Link href="/" >
+            <Link href="/">
             <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
               <ul className="hidden h-full gap-12 lg:flex">
                 {NAV_LINKS.map((link)=> (
@@ -16,7 +16,7 @@ const Navbar = () => {
                     </Link>
                 ))}
               </ul>
-
+                    
             </Link>
 
         </nav>
