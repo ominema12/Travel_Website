512f9bb26a0909105131aac9eccac6fe076f02f8
ominema12 <ominema12@gmail.com>
2023-11-16 19:57:16 +0530

    ,

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,8 +4,10 @@ import Button from './Button'
 
 const Hero = () => {
   return (
-  <section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row border-2 border-red-500">
+  <section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row ">
     <div className="hero-map"/>
+
+
 <div className="relative z-20 flex flex-1 flex-col xl:w-1/2">
 <Image
   src="/camp.svg"
@@ -18,6 +20,8 @@ const Hero = () => {
 <p className="regular-16 mt-6 text-gray-30 xl:max-w-[520px]">
 We want to be in each one of your journeys seeking the satisfaction of seeing the incorruptible beauty of nature. We can help you on an adventure the world in just one app   
 </p>
+
+
 <div className="my-11 flex flex-wrap gap-5">
   <div className="flex items-center gap-2">
   {Array(5).fill(1).map((_,index)=> (
@@ -29,14 +33,15 @@ width={24}
 height={24}
 />
   )) }
-
 </div>
+
 
 <p className="bold-16 lg:bold-20 text-blue-70">
   198k
 <span className="regular-16 lg:regular-20 ml-1">Excellent Reviews</span>
 </p>
 </div>
+
 <div className="flex flex-col w-full gap-3 sm:flex-row">
   <Button 
   type="button" 
@@ -49,9 +54,24 @@ height={24}
   icon="/play.svg"
   variant="btn_white_text"
   />
+  </div>
 </div>
-</div>
-  </section>
+
+<div className="relative flex flex-1 items-start">
+     <div className="relative z-20 flex w-[268px] flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8">
+
+
+
+     <div className="flex flex-col">
+    <div className="flexBetween">
+      <p className="regular-16"></p>
+
+    </div>
+     </div>
+
+     </div>
+     </div>
+  </section> 
   )
 }
 
