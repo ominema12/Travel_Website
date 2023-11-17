a3eb3cd8893a7f18a0a706fd471c0cae695d4c55
ominema12 <ominema12@gmail.com>
2023-11-18 03:16:54 +0530

    done

--- components/Features.tsx
+++ components/Features.tsx
@@ -4,31 +4,34 @@ import Image from 'next/image'
 const Features = () => {
   return (
     <section className="flex flex-col flexCenter overflow-hidden bg-feature-bg bg-center bg-no-repeat py-24" >
-<div className="max-container padding-container relative w-full flex justify-end">
-  <div className="flex flex-1 lg:min-h-[900px]">
-<Image
-src="/phone.png"
-alt="phone"
-width={440}
-height={1000}
-className="features-phone"
-/>
-  </div>
+      <div className="max-container padding-container relative w-full flex justify-end">
+        <div className="flex flex-1 lg:min-h-[900px]">
+          <Image
+            src="/phone.png"
+            alt="phone"
+            width={440}
+            height={1000}
+            className="features-phone"
+          />
+        </div>
 
-<div className="z-20 flex w-full flex-col lg:w-[60%]">
-  <div className="relative">
-    <Image
-    src="/camp.svg"
-    alt="camp"
-    />
+        <div className="z-20 flex w-full flex-col lg:w-[60%]">
+          <div className="relative">
+            <Image
+              src="/camp.svg"
+              alt="camp"
+              width={50}
+              height={50}
+              className="absolute left-[-5px] top-[-28px] w-10 lg:w-[50px]"
+            />
+            <h2 className="bold-40 lg:bold-64">Our Features</h2>
+          </div>
 
-  </div>
+        </div>
+      </div>
 
-</div>
-</div>
-    
 
-     </section>
+    </section>
   )
 }
 
