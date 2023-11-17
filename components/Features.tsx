b595a150fdabc6b0ef33444d0de0bdbe3de6ec2e
ominema12 <ominema12@gmail.com>
2023-11-18 03:11:44 +0530

    done ahead

--- components/Features.tsx
+++ components/Features.tsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Features = () => {
   return (
     <section className="flex flex-col flexCenter overflow-hidden bg-feature-bg bg-center bg-no-repeat py-24" >
+<div className="max-container padding-container relative w-full flex justify-end">
+  <div className="flex flex-1 lg:min-h-[900px]">
+<Image
+src="/phone.png"
+alt="phone"
+width={440}
+height={1000}
+className="features-phone"
+/>
+  </div>
 
+<div className="z-20 flex w-full flex-col lg:w-[60%]">
+  <div className="relative">
+    <Image
+    src="/camp.svg"
+    alt="camp"
+    />
+
+  </div>
+
+</div>
+</div>
     
 
      </section>
