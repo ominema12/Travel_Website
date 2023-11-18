332deadde4e4c9b5f07ef8a4f05ba404e1682077
ominema12 <ominema12@gmail.com>
2023-11-18 23:26:58 +0530

    button updated

--- components/Button.tsx
+++ components/Button.tsx
@@ -14,7 +14,7 @@ const Button = ({type,title,icon,variant}:ButtonProps ) => {
       type={type}
     >
         {icon && <Image src={icon} alt={title} width={24} height={24} />}
-        <label className="bold-16 whitespace-nowrap">{title}</label>
+        <label className="bold-16 whitespace-nowrap cursor">{title}</label>
 
     </button>
   )
