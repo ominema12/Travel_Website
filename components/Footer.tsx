3596cef649b835bb059176905765533315164fd2
ominema12 <ominema12@gmail.com>
2023-11-19 02:45:13 +0530

    done footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,39 @@
+import { FOOTER_CONTACT_INFO, FOOTER_LINKS, SOCIALS } from '@/constants'
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+
 
 const Footer = () => {
   return (
-    <footer>
-      
+    <footer className="flexCenter mb-24">
+      <div className="padding-container max-container flex w-full">
+        <div className="flex flex-col items-start justify-center gap-[10%] md:flex-row">
+          <Link href="/" className="mb-10">
+            <Image src="hilink-logo.svg" alt="logo" width={74} height={29} />
+          </Link>
+  
+          <div className="flex flex-wrap gap-10 sm:justify-between md:flex-1">
+            {FOOTER_LINKS.map((columns) =>(
+              <FooterColumn></FooterColumn>
+            ))}
+          </div>
+
+
+
+        </div>
+      </div>
+
     </footer>
   )
 }
+
+
+type FooterColumnProps={
+  title:string;
+  children:React.ReactNode;
+}
+
+
 
 export default Footer
